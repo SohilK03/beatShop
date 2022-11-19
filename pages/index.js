d1e271649aa338d44ca43d56ca11aa0172d04253
Sohil Khattar <sohilkhattar123@gmail.com>
2022-11-19 17:49:43 +0530

    added files

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,43 @@
-import React from "react";
-import { Product, FooterBanner, HeroBanner } from "../components";
-import { client } from "../lib/client";
+import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client';
+
+
+
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {console.log(bannerData ? bannerData : "fetching")}
-      <div className="products-heading">
-        <h2>Best Selling Products</h2>
+      <HeroBanner  heroBanner={bannerData.length && bannerData[0]}/>
+      <div className='products-heading'>
+        <h2>Best Selling products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div className="products-container">
-        {console.log(products)}
-        {products?.map((product) => product.name)}
+
+      <div className='products-container'>
+        {products?.map((product) => <Product 
+          key={product._id}
+          product={product}
+        />)}
       </div>
-      <FooterBanner />
+      <FooterBanner 
+        footerBanner={bannerData.length && bannerData[0]}
+      />
+      
     </>
-  );
-};
+  )
+}
+
+//fetching data from sanity in nextjs(we use getServerSideProps rather than useeffect in reactjs)
 export const getServerSideProps = async () => {
-  const query = '*[_type=="product"]';
+  const query = '*[_type == "product"]';
   const products = await client.fetch(query);
-  const bannerQuery = '*[_type=="banner"]';
+
+  const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
+
   return {
-    props: { products, bannerData },
-  };
-};
+    props: { products, bannerData }
+  }
+}
+
 export default Home;
